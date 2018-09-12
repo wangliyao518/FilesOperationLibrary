df151e9126bb6482cf0659eaba6a7a12cf44a879
liyawang <leo.y.wang@nokia-sbell.com>
2018-09-12 22:14:58 +0800

    change readme file suffix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 CURDIR = dirname(abspath(__file__))
 REQUIREMENTS = ['robotframework >= 2.0']
-with open(join(CURDIR, 'README.rst')) as f:
+with open(join(CURDIR, 'README.md')) as f:
     DESCRIPTION = f.read()
 CLASSIFIERS = '''
 Development Status :: 5 - Production/Stable
